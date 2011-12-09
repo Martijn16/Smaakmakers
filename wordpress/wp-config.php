a40b37c3b2bb4d45dc3dd4693508c6ea756b1993
Martijn Post <martijnpost16@hotmail.com>
2011-12-09 13:16:53 +0100

    Installed wordpress

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'vandeleur');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
