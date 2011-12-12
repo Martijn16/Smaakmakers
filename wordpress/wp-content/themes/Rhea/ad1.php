c53a7d21147959ef93602deae893cedcf4927e5d
Bouke Korf <bouke.korf@live.nl>
2011-12-12 12:34:35 +0100

    Database opzet bouke

--- wordpress/wp-content/themes/Rhea/ad1.php
+++
@@ -1,4 +0,0 @@
-<div class="widebanner">
-<?php $ad1 = get_option('rhea_ad1'); echo stripslashes($ad1); ?>
-</div>
-<div class="clear"></div>
--- wordpress/wp-content/themes/Rhea/js/effects.js
+++
@@ -1,3 +0,0 @@
-jQuery(document).ready(function() {
-Cufon.replace('.blogname h1,.title h2,h2.pagetitle,h3.sidetitl', { fontFamily: 'Qlassik Bold' });
-});
